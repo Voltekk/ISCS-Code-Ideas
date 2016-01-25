6abee98bd3cd5a75abecdb6a5847366ef5773078
Regiment <jwningli@gwu.edu>
2016-01-25 11:03:17 -0500

    Create ip_domain_lookup.pl

---
+++ ip_domain_lookup.pl
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+
+while(){
+	my $what_to_query = &query("Enter 1 for IP 2 for domain");
+	if ($what_to_query !~ /1|2/){
+		print "Invalid entry";
+		next;
+	}
+	elsif($what_to_query =~ /1/){
+		my $ip = &query("Enter IP");
+		my $domaintools = "whois.domaintools.com/".$ip;
+		my $virustotal = "https://www.virustotal.com/en/ip-address/".$ip."/information/";
+		my $command = "chrome ";
+		system($command.$domaintools);
+		system($command.$virustotal);
+	}
+	elsif($what_to_query =~ /2/){
+		my $domain = &query("Enter domain");
+		my $domaintools = "whois.domaintools.com/".$domain;
+		my $virustotal = "https://www.virustotal.com/en/domain/".$domain."/information/";
+		my $command = "chrome ";
+		system($command.$domaintools);
+		system($command.$virustotal);
+	}
+	else{
+		next;
+	}
+}
+
+sub query(){
+	my $question = shift;
+	print "\n".$question.":\t";
+	my $input = <STDIN>;
+	chomp $input;
+	return $input;
+	undef $input;
+	undef $question;
+}
